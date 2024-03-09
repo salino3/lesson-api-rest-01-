5f4554880a5ff6105130cd0d6ed592762301a1ed
salino3 <programacionflavio@gmail.com>
2024-03-09 09:52:24 +0100

    commit

--- src/dals/house/repositories/index.ts
+++ src/dals/house/repositories/index.ts
@@ -1,6 +1,6 @@
-import { mockRepository } from "./movie.mock-repository.js";
-import { dbRepository } from "./movie.db-repository.js";
-import { envConstants } from "#core/constants/index.js";
+import { envConstants } from '#core/constants/index.js';
+import { dbRepository } from './house.db-repository.js';
+import { mockRepository } from './house.mock-repository.js';
 
 export const movieRepository = envConstants.isApiMock
   ? mockRepository
