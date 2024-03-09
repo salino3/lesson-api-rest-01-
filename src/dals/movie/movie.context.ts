4f1534bade65a10afce1a16ccd399125f2961e4d
salino3 <programacionflavio@gmail.com>
2024-03-09 09:33:57 +0100

    interfaces

--- src/dals/movie/movie.context.ts
+++ src/dals/movie/movie.context.ts
@@ -1,4 +1,4 @@
 import { db } from '#core/servers/index.js';
-import { Movie } from './movie.model.js';
+import { Movie } from './house.model.js';
 
 export const getMovieContext = () => db?.collection<Movie>('movies');
